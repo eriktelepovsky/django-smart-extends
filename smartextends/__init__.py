2ee520c67a325e820abd1150b735d8aa407be862
Erik Telepovský <erik.telepovsky@gmail.com>
2015-07-07 14:21:52 +0200

    support for Django 1.7

--- smartextends/__init__.py
+++ smartextends/__init__.py
@@ -1,4 +1,6 @@
-from django.template.loader import add_to_builtins
-
+try:
+    from django.template.loader import add_to_builtins
+except ImportError:
+    from django.template.base import add_to_builtins
 
 add_to_builtins('smartextends.templatetags.smart_extends_tags')
